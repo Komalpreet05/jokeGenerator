ece1189d31b7f6d2922bc94e44e2c3579396663f
Komalpreet Singh <komalpreetsingh2305@gmail.com>
2024-03-05 18:10:10 -0500

    final version

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const jokeContainer = document.querySelector("#joke");
 const btn = document.getElementById("generate");
 const language = document.querySelector("#language");
+const loading = document.querySelector(".option2");
+//const loading = document.getElementsByClassName("option2");
+const result = document.querySelector(".option1");
+console.log(loading);
 console.log(language.value);
 const urlEng = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single";
 const urlHin = "https://hindi-jokes-api.onrender.com/jokes?api_key=4a160d0499f1efc3c39203f9ba7c";
@@ -10,19 +14,62 @@ const urlHin = "https://hindi-jokes-api.onrender.com/jokes?api_key=4a160d0499f1e
 // })
 
 async function checkHin() {
-    jokeContainer.classList.remove("fade");
-    const res = await fetch(urlHin);
-    const data = await res.json();
-    jokeContainer.textContent = data.jokeContent;
-    jokeContainer.classList.add("fade");
+    //jokeContainer.classList.remove("fade");
+    result.classList.remove("active");
+    loading.classList.add("active");
+    if (loading.classList.contains("active")) {
+        jokeContainer.style.display = "none";
+        console.log("if");
+    }
+
+
+    try {
+        const res = await fetch(urlHin);
+        const data = await res.json();
+        loading.classList.remove("active");
+        if (!(loading.classList.contains("active"))) {
+            jokeContainer.style.display = "flex";
+            console.log("else if");
+        }
+        jokeContainer.textContent = data.jokeContent;
+
+        result.classList.add("fade");
+        console.log("test");
+        result.classList.add("active");
+    }
+    catch (err) {
+
+    }
 }
 
 async function checkEng() {
-    jokeContainer.classList.remove("fade");
-    const res = await fetch(urlEng);
-    const data = await res.json();
-    jokeContainer.textContent = data.joke;
-    jokeContainer.classList.add("fade");
+    //jokeContainer.classList.remove("fade");
+    result.classList.remove("active");
+    loading.classList.add("active");
+    if (loading.classList.contains("active")) {
+        jokeContainer.style.display = "none";
+        console.log("if");
+    }
+    try {
+        const res = await fetch(urlEng);
+        const data = await res.json();
+        loading.classList.remove("active");
+        if (!(loading.classList.contains("active"))) {
+            jokeContainer.style.display = "flex";
+            console.log("else if");
+        }
+        jokeContainer.textContent = data.joke;
+        result.classList.add("fade");
+        console.log("test");
+        result.classList.add("active");
+    }
+    catch (err) {
+
+
+    }
+
+    //jokeContainer.classList.add("fade");
+
 }
 
 
